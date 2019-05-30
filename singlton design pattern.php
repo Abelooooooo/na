b49ebc38202ab71757ec0953e2b5f839f1b22279
Abelooooooo <abelgutu5@gmail.com>
2019-05-30 12:47:29 -0700

    intal commit

---
+++ singlton design pattern.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Singleton Design Pattern
+
+*/
+class Singleton
+{
+    
+    private static $instance;
+    
+   
+    public static function getInstance()
+    {
+        if (null === static::$instance) {
+            static::$instance = new static();
+        }
+        
+        return static::$instance;
+    }
+    
+    protected function __construct()
+    {
+    }
+   
+    private function __clone()
+    {
+    }
+  
+    private function __wakeup()
+    {
+    }
+}
+class SingletonChild extends Singleton
+{
+}
+$obj = Singleton::getInstance();
+var_dump($obj === Singleton::getInstance());             
+$anotherObj = SingletonChild::getInstance();
+var_dump($anotherObj === Singleton::getInstance());      
+var_dump($anotherObj === SingletonChild::getInstance()); 
